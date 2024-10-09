fca6864fb6c4ee694c386207d380e2ebc5ac00a6
ahmadhakimitsnaini <ahmadhakimitsnaini@outlook.com>
2024-10-09 21:07:31 +0700

    Building a date formatter step 20

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
 
+// Get value date use new object
 const date = new Date();
 const day = date.getDate();
 const month = date.getMonth() + 1;
@@ -8,4 +9,15 @@ const year = date.getFullYear();
 const hours = date.getHours();
 const minutes = date.getMinutes();
 
-const formattedDate = `${day}`;
+const formattedDate = `${day}-${month}-${year}`;
+currentDateParagraph.textContent = formattedDate;
+
+const exampleSentence = "selur pmaCedoCeerf".split("").reverse();
+console.log(exampleSentence);
+dateOptionsSelectElement.addEventListener("change", () => {
+  // When a user makes a selection from the dropdown menu, the function should get the user's value and display the date in their chosen date format. To do this, you can use the switch statement.
+  switch (dateOptionsSelectElement.value) {
+    case "yyyy-mm-dd":
+      currentDateParagraph.textContent = formattedDate;
+  }
+});
