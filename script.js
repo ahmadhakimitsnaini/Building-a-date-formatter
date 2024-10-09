75732cf69a09ec840110ed9903c0824ffe370ad3
ahmadhakimitsnaini <ahmadhakimitsnaini@outlook.com>
2024-10-09 21:35:26 +0700

    Building a date formatter Finish

--- script.js
+++ script.js
@@ -12,12 +12,23 @@ const minutes = date.getMinutes();
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
 
-const exampleSentence = "selur pmaCedoCeerf".split("").reverse();
-console.log(exampleSentence);
 dateOptionsSelectElement.addEventListener("change", () => {
   // When a user makes a selection from the dropdown menu, the function should get the user's value and display the date in their chosen date format. To do this, you can use the switch statement.
   switch (dateOptionsSelectElement.value) {
     case "yyyy-mm-dd":
+      currentDateParagraph.textContent = formattedDate
+        .split("-")
+        .reverse()
+        .join("-");
+      break;
+    case "mm-dd-yyyy-h-mm":
+      currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+      break;
+    default:
       currentDateParagraph.textContent = formattedDate;
   }
 });
+
+/*
+    split, join, reverse method explain in chapter 22
+ */
